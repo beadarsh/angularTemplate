fd93b5ae9816fd64e5d5b6144cfed69e4437c09b
beadarsh <beadarsh@gmail.com>
2018-07-21 19:02:14 +0100

    Removed un necessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GenCodeComponent } from './gen-code/gen-code.component';
-import { UpdateOnFlyGithubComponent } from './update-on-fly-github/update-on-fly-github.component';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { TestComponent } from './test/test.component';
 
@@ -29,7 +28,6 @@ import { FormUiComponent } from 'src/app/form-ui/form-ui.component';
     AppComponent,
     StackblitzComponent,
     GenCodeComponent,
-    UpdateOnFlyGithubComponent,
     TestComponent,
     MyNavComponent,
     NavItemComponent,
